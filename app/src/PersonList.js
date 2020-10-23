75f527fbc9a74476436c15ac8c50df98ae0c7787
westmoso <westmoso@gmail.com>
2020-10-22 20:25:01 -0400

    testing get and post apis.

--- app/src/PersonList.js
+++ app/src/PersonList.js
@@ -1,36 +1,64 @@
 import React from 'react';
 import axios from 'axios';
 
+import API from '../api';
+
 export default class PersonList extends React.Component {
-  state = {
-    id: '',
+    state = {
+      persons: []
+    }
+  
+    componentDidMount() {
+      axios.get(`http://localhost:3000/get`)
+        .then(res => {
+          const persons = res.data;
+          this.setState({ persons });
+        })
+    }
+  
+    render() {
+      return (
+        <ul>
+          { this.state.persons.map(person => <li>{person.name}</li>)}
+        </ul>
+      )
+    }
   }
 
-  handleChange = event => {
-    this.setState({ id: event.target.value });
+  export default class PersonList extends React.Component {
+    state = {
+      name: '',
+    }
+  
+    handleChange = event => {
+      this.setState({ name: event.target.value });
+    }
+  
+    handleSubmit = event => {
+      event.preventDefault();
+  
+      const user = {
+        name: this.state.name
+      };
+  
+      axios.post(`http://localhost:3000/post`, { user })
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+    }
+  
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Person Name:
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      )
+    }
   }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    axios.delete(`http://localhost:3000/delete${this.state.id}`)
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person ID:
-            <input type="text" name="id" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Delete</button>
-        </form>
-      </div>
-    )
-  }
-}

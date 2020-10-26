099ce1d4973870ba6527fdcdc4165a8661cfa3c9
westmoso <westmoso@gmail.com>
2020-10-26 17:02:05 -0400

    removed unneeded code in navbar..

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -11,9 +11,3 @@ function navBar () {
 }
 
 export default navBar;
-
-{/* <section className="navbar">
-<a href="/" className="navbar-item">Home</a>
-<a href="/" className="navbar-item">Retrieve Music</a>
-<a href="/search" className="navbar-item">Search</a>
-</section> */}
